fabf9f431383d97b4d8149f37f1e99eddb388569
94onur94 <onuur94@hotmail.com>
2019-06-27 11:12:20 +0300

    n/a

---
+++ labelling_csv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+from PIL import Image
+import os, sys, xlwt
+from pathlib import Path
+
+workbook = xlwt.Workbook()
+
+sheet = workbook.add_sheet("Sheet Name")
+style_red = xlwt.easyxf('font: bold 1, color red;')
+style_blue = xlwt.easyxf('font: color blue;')
+style_bold = xlwt.easyxf('font: bold 1')
+
+folder_name = "Cars_test"
+
+strPath = str(Path(__file__).resolve().parent) + "\\" + folder_name
+
+if os.path.isdir(strPath):
+    print("Folder found")
+
+size = len(os.listdir(strPath))
+
+def resize():
+    column=1
+    row=1
+    for folders in os.listdir(strPath):
+        sheet.write(0, column, folders, style_bold)
+        print(folders)
+        for files in os.listdir(strPath + "\\" + folders):
+            sheet.write(row, 0, files)
+            for point in range (1,size+1):
+                if point == column:
+                    sheet.write(row, point, 1)
+                else:
+                    sheet.write(row, point, 0)
+            row += 1
+        column += 1
+    workbook.save("labels.csv")
+
+resize()
+print("---Finished---")
